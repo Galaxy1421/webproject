b274f44ac905d5473d6a05d892e4614b5c4a8502
Renadalju <124964163+Renadalju@users.noreply.github.com>
2024-04-27 02:51:52 +0300

    Add files via upload

---
+++ JSProject.js
@@ -0,0 +1,26 @@
+	
+
+
+
+window.addEventListener("load",start,false);
+
+function start(){
+
+  
+
+   document.getElementById("buttonClicked").addEventListener("click",popup,false);
+   document.getElementById("buttonClicked2").addEventListener("click",popup,false);
+   document.getElementById("buttonClicked3").addEventListener("click",popup,false);
+   document.getElementById("buttonClicked4").addEventListener("click",popup,false);
+   document.getElementById("buttonClicked5").addEventListener("click",popup,false);
+   document.getElementById("buttonClicked6").addEventListener("click",popup,false);
+
+
+}
+
+function popup(){
+
+   alert("Are you ready to read more?");
+
+}
+
